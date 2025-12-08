4db0e0d7e756ad4f35dfc46299f75bc5b36b02e9
nikitakgu <_email@example.com>
2025-12-09 02:03:16 +0300

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+﻿import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/lab1-react-vite-gh-pages/",
   plugins: [react()],
-  // @ts-ignore - Vite types don't include test property
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
   },
-})
+});
